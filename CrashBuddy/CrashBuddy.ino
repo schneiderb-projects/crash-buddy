339ea2521d3db7e7ae40e2ed8b01e8efac347cf8
Brett Schneider <59781241+schneiderb-projects@users.noreply.github.com>
2022-11-29 13:46:58 -0500

    Update CrashBuddy.ino

--- CrashBuddy/CrashBuddy.ino
+++ CrashBuddy/CrashBuddy.ino
@@ -5,17 +5,21 @@ Crash Buddy: impact detection
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL375.h>
 #include "RingBuffer.h"
 
 //sensor reading/crash detection related variables
-#define DEFAULT_THRESHHOLD 19
+//#define DEFAULT_THRESHHOLD 0
+#define DEFAULT_THRESHHOLD 15
 volatile uint16_t new_data = 0;
-volatile uint16_t total_new_data = 0; 
+volatile uint16_t total_new_data = 0;
 bool crash_detected = false;
 
 //BLE Gatt Server UUIDS, all letters must be lowercase
 #define PREAMBLE "0998"
-#define DEVICE_UUID  "-1280-49a1-bacf-965209262e66" // last 14 bytes of char uuid
+#define DEVICE_UUID "-1280-49a1-bacf-965209262e66"  // last 14 bytes of char uuid
 //#define DEVICE_UUID  "-1280-49A1-BACF-965209262E66" // last 14 bytes of char uuid
 
 //UUIDS for service and characteristics
@@ -29,18 +33,18 @@ bool crash_detected = false;
 #define CHARACTERISTIC_UUID_CRASH_DATA_CHAR_SIZE PREAMBLE "0008" DEVICE_UUID
 
 //crash data characteristics: must edit these by hand to change the number of datapoints sent
-#pragma pack(1) // prevent compiler adding padding for 32 bit alignment
+#pragma pack(1)      // prevent compiler adding padding for 32 bit alignment
 struct data_point {  // total size = 6 bytes
   int16_t value;     // value recorded by accelerometer
   int32_t time;      // clock time, not real time
 };
 
 #define CHARACTERISTIC_UUID_CRASH_DATA_INITIAL 0x0010  //initial address for Crash Data Characteristics
-#define TOTAL_CRASH_DATAPOINTS 10                      // Total number of crash data datapoints
-#define SIZEOF_DATA_POINT sizeof(struct data_point)                      // sizeof(struct data_point)
-#define MAX_BYTES_PER_CHAR 600
-#define DATA_POINTS_PER_CHAR (MAX_BYTES_PER_CHAR / SIZEOF_DATA_POINT)  // Datapoints stored in one characteristic
-#define CRASH_DATA_CHARS  (TOTAL_CRASH_DATAPOINTS / MAX_BYTES_PER_CHAR + 2) // number of crash data characteristics
+#define TOTAL_CRASH_DATAPOINTS 2000                    // Total number of crash data datapoints
+#define SIZEOF_DATA_POINT sizeof(struct data_point)    // sizeof(struct data_point)
+#define MAX_BYTES_PER_CHAR 516
+#define DATA_POINTS_PER_CHAR (MAX_BYTES_PER_CHAR / SIZEOF_DATA_POINT)       // Datapoints stored in one characteristic
+#define CRASH_DATA_CHARS (TOTAL_CRASH_DATAPOINTS / DATA_POINTS_PER_CHAR + 2)  // number of crash data characteristics
 
 #define UUID_COUNT (9 + CRASH_DATA_CHARS)
 
@@ -61,7 +65,7 @@ BLECharacteristic* pCrashDataCharacteristics[CRASH_DATA_CHARS];
 
 //BLE GATT characteristic values
 uint32_t statusValue = 0;
-uint32_t* threshholdValue;
+uint32_t threshholdValue = DEFAULT_THRESHHOLD;
 uint8_t* setEnableDebugValue;
 
 //data storage related variables
@@ -69,20 +73,22 @@ struct data_point data_buffer[TOTAL_CRASH_DATAPOINTS] = { 0 };
 
 // Should fake data be used?
 
-portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
-#define USE_FAKE_DATA // Define to use fake data instead of reading from the sensor
-#define USE_FAKE_BATCHED_DATA // Must also define use USE_FAKE_DATA otherwise this is ignored 
-#ifdef USE_FAKE_DATA 
-// fake data variables 
-hw_timer_t * timer = NULL;
 struct data_point temp_data = { 0, 0 };
-#else 
+//#define USE_FAKE_DATA // Define to use fake data instead of reading from the sensor
+//#define USE_FAKE_BATCHED_DATA // Must also define use USE_FAKE_DATA otherwise this is ignored
+#ifdef USE_FAKE_DATA
+// fake data variables
+hw_timer_t* timer = NULL;
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+#else
 // real data variables
+Adafruit_ADXL375 accel = Adafruit_ADXL375(12345);
+long last_read = 0;
 #endif
 
 RingBuffer rb(data_buffer, TOTAL_CRASH_DATAPOINTS * sizeof(struct data_point), sizeof(struct data_point));
 
-void sensor_read(); // sensor read declaration
+void sensor_read();  // sensor read declaration
 
 //BLE Gatt Table Callbacks
 class MyServerCallbacks : public BLEServerCallbacks {
@@ -98,18 +104,24 @@ class MyServerCallbacks : public BLEServerCallbacks {
 class MyCallbacks : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic* pCharacteristic) {
     if (pCharacteristic->getValue().length() > 0) {
-      // Serial.print("*********");
-      // Serial.write(pCharacteristic->getUUID().toString().c_str());
-      // Serial.println("*********");
-      // Serial.print("Received Value: ");
-      // for (int i = 0; i < value.length(); i++)
-      //   Serial.print(value[i]);
+      Serial.print("*********");
+      Serial.write(pCharacteristic->getUUID().toString().c_str());
+      Serial.println("*********");
+      Serial.print("********* Length: ");
+      Serial.print(pCharacteristic->getValue().length());
+      Serial.println("*********");
+      Serial.print("Received Value: ");
+      for (int i = 0; i < pCharacteristic->getValue().length(); i++)
+        Serial.print((uint8_t) pCharacteristic->getValue()[i]); Serial.print(" ");
 
-      // Serial.println();
-      // Serial.println("*********");
+      Serial.println();
+      Serial.println("*********");
 
       if (pCharacteristic->getUUID().toString() == CHARACTERISTIC_UUID_SET_THRESHHOLD) {
-        threshholdValue = (uint32_t *) pCharacteristic->getData();
+        // threshholdValue = 0;
+        // threshholdValue = pCharacteristic->getData()[4];
+        // Serial.print(threshholdValue); Serial.print(" ");
+        // Serial.print("\nNew Threshhold: "); Serial.println(threshholdValue);
       }
 
       else if (pCharacteristic->getUUID().toString() == CHARACTERISTIC_UUID_SET_ENABLE_DEBUG) {
@@ -124,57 +136,57 @@ void create_characteristics(BLEService* pService) {
   pStatusCharacteristic = pService->createCharacteristic(
     CHARACTERISTIC_UUID_STATUS,
     BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
-  pStatusCharacteristic->addDescriptor(new BLE2902());    
+  pStatusCharacteristic->addDescriptor(new BLE2902());
 
-  pStatusCharacteristic->setValue((uint8_t*) &statusValue, sizeof(statusValue));
+  pStatusCharacteristic->setValue((uint8_t*)&statusValue, sizeof(statusValue));
 
   //data available
   pDataAvailableCharacteristic = pService->createCharacteristic(
     CHARACTERISTIC_UUID_DATA_AVAILABLE,
     BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
-  pDataAvailableCharacteristic->addDescriptor(new BLE2902());    
+  pDataAvailableCharacteristic->addDescriptor(new BLE2902());
 
   uint32_t dataAvailableValue = 0;
-  pDataAvailableCharacteristic->setValue((uint8_t*) &dataAvailableValue, sizeof(dataAvailableValue));
+  pDataAvailableCharacteristic->setValue((uint8_t*)&dataAvailableValue, sizeof(dataAvailableValue));
 
   //data characteristic size
   pDataSizeCharacteristic = pService->createCharacteristic(
     CHARACTERISTIC_UUID_DATA_SIZE,
     BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
-  pDataSizeCharacteristic->addDescriptor(new BLE2902());    
+  pDataSizeCharacteristic->addDescriptor(new BLE2902());
 
 
-  uint32_t dataSizeValue = TOTAL_CRASH_DATAPOINTS;  
+  uint32_t dataSizeValue = TOTAL_CRASH_DATAPOINTS;
   pDataSizeCharacteristic->setValue((uint8_t*)&dataSizeValue, sizeof(dataSizeValue));
 
   //set threshhold
   pSetThreshholdCharacteristic = pService->createCharacteristic(
     CHARACTERISTIC_UUID_SET_THRESHHOLD,
     BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_READ);
-  pSetThreshholdCharacteristic->addDescriptor(new BLE2902());    
+  pSetThreshholdCharacteristic->addDescriptor(new BLE2902());
 
   pSetThreshholdCharacteristic->setCallbacks(new MyCallbacks());
 
-  uint32_t threshhold = DEFAULT_THRESHHOLD; 
-  pSetThreshholdCharacteristic->setValue((uint8_t*) &threshhold, sizeof(threshhold));
-  threshholdValue = (uint32_t *) pSetThreshholdCharacteristic->getData();
+  uint32_t threshhold = DEFAULT_THRESHHOLD;
+  pSetThreshholdCharacteristic->setValue((uint8_t*)&threshhold, sizeof(threshhold));
+  //threshholdValue = *((uint32_t*)pSetThreshholdCharacteristic->getData());
 
   //set enable debug
   pSetEnableDebugCharacteristic = pService->createCharacteristic(
     CHARACTERISTIC_UUID_SET_ENABLE_DEBUG,
     BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_READ);
-  pSetEnableDebugCharacteristic->addDescriptor(new BLE2902());    
+  pSetEnableDebugCharacteristic->addDescriptor(new BLE2902());
 
   pSetEnableDebugCharacteristic->setCallbacks(new MyCallbacks());
 
-  uint8_t setEnableDebug = 0; 
-  pSetEnableDebugCharacteristic->setValue((uint8_t*) &setEnableDebug, sizeof(setEnableDebug));
+  uint8_t setEnableDebug = 0;
+  pSetEnableDebugCharacteristic->setValue((uint8_t*)&setEnableDebug, sizeof(setEnableDebug));
 
   // crash data char size
   pCrashDataCharSizeCharacteristic = pService->createCharacteristic(
     CHARACTERISTIC_UUID_CRASH_DATA_CHAR_SIZE,
     BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
-  pCrashDataCharSizeCharacteristic->addDescriptor(new BLE2902());    
+  pCrashDataCharSizeCharacteristic->addDescriptor(new BLE2902());
 
   uint32_t dataCharSizeValue = DATA_POINTS_PER_CHAR;
   pCrashDataCharSizeCharacteristic->setValue((uint8_t*)&dataCharSizeValue, sizeof(dataCharSizeValue));
@@ -197,7 +209,6 @@ void create_characteristics(BLEService* pService) {
       uuid,
       BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_READ);
     pCrashDataCharacteristics[i]->addDescriptor(new BLE2902());
-
   }
 }
 
@@ -205,19 +216,20 @@ void create_characteristics(BLEService* pService) {
 void setup() {
   Serial.begin(115200);
 
-  Serial.print("Service UUID: ");
-  Serial.println(SERVICE_UUID);
-  #ifdef USE_FAKE_DATA
+#ifdef USE_FAKE_DATA
   //enable a timer interrupt in place of the pin interrupt when using an actual sensor
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &sensor_read, true);
   timerAlarmWrite(timer, 500000, true);
   timerAlarmEnable(timer);
-  #else
-  #endif
+#else
+  // enable sensor
+  if (!accel.begin()) {
+    // TODO: uh oh
+  }
+#endif
 
-  // Create the BLE Device
-  BLEDevice::init("Crash Buddy");
+  BLEDevice::init("ESP32");
 
   // Create the BLE Server
   pServer = BLEDevice::createServer();
@@ -240,33 +252,50 @@ void setup() {
 
   // Start advertising
   pServer->getAdvertising()->start();
+  Serial.print("Threshhold: "); Serial.println(threshholdValue);
   Serial.println("Ready for connection...");
 }
 
 void sensor_read() {
 #ifdef USE_FAKE_DATA
-    // timer used for fake data
-    portENTER_CRITICAL_ISR(&mux);
+  // For fake data
+  portENTER_CRITICAL_ISR(&mux);
 #ifdef USE_FAKE_BATCHED_DATA
-    int size = min((int) DATA_POINTS_PER_CHAR, TOTAL_CRASH_DATAPOINTS);
-    for (int i = 0; i < size; i++) {
-      temp_data.time++;
-      temp_data.value = (temp_data.value + 1) % 40;
-      rb.push(&temp_data);
-    }
-
-    new_data = size;
-#else
-    rb.push(&temp_data);
+  int size = min((int)DATA_POINTS_PER_CHAR, TOTAL_CRASH_DATAPOINTS);
+  for (int i = 0; i < size; i++) {
     temp_data.time++;
-    temp_data.value = (temp_data.value + 1) % 20;
-    new_data += 1;
+    temp_data.value = (temp_data.value + 1) % 40;
+    rb.push(&temp_data);
+  }
+
+  new_data += size;
+#else
+  rb.push(&temp_data);
+  temp_data.time++;
+  temp_data.value = (temp_data.value + 1) % 20;
+  new_data += 1;
 
 #endif
-    total_new_data += new_data;
-    portEXIT_CRITICAL_ISR(&mux);
+  total_new_data += new_data;
+  portEXIT_CRITICAL_ISR(&mux);
 #else
   // actual sensor reading
+  if (millis() - last_read > 10) {
+    sensors_event_t event;
+    accel.getEvent(&event);
+    temp_data.time = (int32_t)millis();
+    temp_data.value = (int16_t)(10 * sqrt(event.acceleration.x * event.acceleration.x + event.acceleration.y * event.acceleration.y + event.acceleration.z * event.acceleration.z) * 0.101971621);
+    rb.push(&temp_data);
+    if (temp_data.value > threshholdValue) {
+      Serial.print(temp_data.time);
+      Serial.print(" ");
+      Serial.println(temp_data.value);
+    }
+
+    new_data += 1;
+    total_new_data += new_data;
+    last_read = millis();
+  }
 #endif
 }
 
@@ -278,36 +307,59 @@ void update_notify(BLECharacteristic* c, void* value, int num_bytes) {
 void ble_notify_new_data() {
   uint8_t val = *pDataAvailableCharacteristic->getData() + 1;
   pDataAvailableCharacteristic->setValue(&val, 1);
+  pDataAvailableCharacteristic->notify();
 }
 
 void crash_detect() {
   struct data_point* temp;
   for (int i = 0; i < new_data; i++) {
-      temp = (struct data_point *) rb.get_at_index(TOTAL_CRASH_DATAPOINTS - new_data + i);
-      if (temp->value >= *threshholdValue && !crash_detected) {
-        crash_detected = 1; 
-        total_new_data = 0;
-      }
+    temp = (struct data_point*)rb.get_at_index(TOTAL_CRASH_DATAPOINTS - new_data + i);
+    if (temp->value >= threshholdValue && !crash_detected) {
+      crash_detected = 1;
+      total_new_data = 0;
+    }
   }
   new_data = 0;
 }
 
 //TODO: fix data race to fill characteristics before next sensor interupt
 void fill_data_characteristics() {
-    uint index = rb.get_index() / sizeof(struct data_point);  
-    uint data_remaining = TOTAL_CRASH_DATAPOINTS;  
-    uint size;
-    for (int i = 0; i < CRASH_DATA_CHARS; i++) {
-      size = min(min(TOTAL_CRASH_DATAPOINTS - index, DATA_POINTS_PER_CHAR), data_remaining);  
-      pCrashDataCharacteristics[i]->setValue((uint8_t *) &data_buffer[index], size * sizeof(struct data_point));
-      data_remaining -= size;
-      index = (index + size) % TOTAL_CRASH_DATAPOINTS;  
-    }
+  uint index = rb.get_index() / sizeof(struct data_point);
+  uint data_remaining = TOTAL_CRASH_DATAPOINTS;
+  uint size;
+  for (int i = 0; i < CRASH_DATA_CHARS; i++) {
+    size = min(min(TOTAL_CRASH_DATAPOINTS - index, DATA_POINTS_PER_CHAR), data_remaining);
+    pCrashDataCharacteristics[i]->setValue((uint8_t*)&data_buffer[index], size * sizeof(struct data_point));
+    data_remaining -= size;
+    index = (index + size) % TOTAL_CRASH_DATAPOINTS;
+  }
+  
+  /*Serial.println("Sending");
+  for(int i = 0; i < TOTAL_CRASH_DATAPOINTS; i++) {
+    uint8_t * val = (uint8_t*)rb.get_at_index(i);
+    Serial.print(i); Serial.print(": "); Serial.print(val[0], HEX); Serial.print(" ");
+    Serial.print(val[1], HEX); Serial.print(" ");
+    Serial.print(val[2], HEX); Serial.print(" ");  
+    Serial.print(val[3], HEX); Serial.print(" ");
+    Serial.print(val[4], HEX); Serial.print(" ");
+    Serial.println(val[5], HEX);
+  }
+
+  for(int i = 0; i < TOTAL_CRASH_DATAPOINTS; i++) {
+    struct data_point * val = (struct data_point*)rb.get_at_index(i);
+    Serial.print(i); Serial.print(", time: "); Serial.print(val->time); 
+    Serial.print(", value: "); Serial.println(val->value);
+  }*/
 }
 
 void loop() {
   if (deviceConnected) {
-    // delay(100); // let the BLE driver run
+  }
+
+  if (threshholdValue != 0) {
+#ifndef USE_FAKE_DATA
+    sensor_read();
+#endif
   }
 
   // disconnecting
@@ -324,7 +376,7 @@ void loop() {
   }
 
   if (new_data) {
-    if(!crash_detected) {
+    if (!crash_detected) {
       crash_detect();
 
       /*for (int i = 0; i < TOTAL_CRASH_DATAPOINTS; i++) {
@@ -333,17 +385,20 @@ void loop() {
         Serial.print(data_buffer[i].time);
         Serial.print(" | ");
       }
-      Serial.println();
-      
+      Serial.println();*/
+
       if (crash_detected) {
-        Serial.println("Crash Detected!");       
-      }*/
+        Serial.println("Crash Detected!");
+      }
     }
 
-    if(crash_detected && (total_new_data > (TOTAL_CRASH_DATAPOINTS / 2))) {
+    if (crash_detected && (total_new_data > (TOTAL_CRASH_DATAPOINTS / 2))) {
       fill_data_characteristics();
-      ble_notify_new_data();      
+      ble_notify_new_data();
+      Serial.println("Crash Notified!");
       crash_detected = 0;
+      total_new_data = 0;
+      new_data = 0;
     }
   }
 }
